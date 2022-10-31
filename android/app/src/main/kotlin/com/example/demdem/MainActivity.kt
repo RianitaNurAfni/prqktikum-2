0d20ecaf652950b47dc5425d75c02110fb2a3f28
Rianita Afni <rianitaafni@example.com>
2022-10-31 19:10:38 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/demdem/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demdem
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:demdem/screen/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen());
+  }
+}
